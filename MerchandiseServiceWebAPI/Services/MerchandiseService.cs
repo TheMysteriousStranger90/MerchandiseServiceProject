b15e0d48ec170acf292d7bff475066a0b8593af7
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2022-09-19 08:27:55 +0300

    add MerchandiseService

---
+++ MerchandiseServiceWebAPI/Services/MerchandiseService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MerchandiseServiceModels;
+using MerchandiseServiceWebAPI.Services.Interfaces;
+
+namespace MerchandiseServiceWebAPI.Services
+{
+    public class MerchandiseService : IMerchandiseService
+    {
+        private readonly GetOrderStateResponse _orderStateResponse = new GetOrderStateResponse(OrderStatus.Other);
+
+        public Task<GetMerchResponse> GetMerchandise(GetMerchRequest request, CancellationToken _)
+        {
+            var response = new GetMerchResponse(new MerchOrder(1,
+                new List<MerchItem>()
+                    { new MerchItem(request.MerchItem.Id, request.MerchItem.ItemName, request.MerchItem.Quantity) }));
+            return Task.FromResult(response);
+        }
+
+        public Task<GetOrderStateResponse> GetMerchandiseOrderState(GetOrderStateRequest request, CancellationToken _)
+        {
+            return Task.FromResult(_orderStateResponse);
+        }
+    }
+}
