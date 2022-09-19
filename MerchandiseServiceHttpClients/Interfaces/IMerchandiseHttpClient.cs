05baea61152c26025823358be073d43b68f6ac89
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2022-09-19 10:52:56 +0300

    fix && add MerchandiseHttpClient v.2

---
+++ MerchandiseServiceHttpClients/Interfaces/IMerchandiseHttpClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MerchandiseServiceModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MerchandiseServiceHttpClients.Interfaces
+{
+    public interface IMerchandiseHttpClient
+    {
+        [HttpGet("{id:long}/{itemName}")]
+        Task<GetMerchResponse> GetMerch([FromRoute] long employeeId, [FromRoute]string itemName, CancellationToken token);
+        
+        [HttpGet]
+        Task<GetOrderStateResponse> GetMerchOrderState([FromQuery]long id, CancellationToken token);
+    }
+}
---
+++ MerchandiseServiceHttpClients/MerchandiseHttpClient.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using MerchandiseServiceHttpClients.Interfaces;
+using MerchandiseServiceModels;
+
+namespace MerchandiseServiceHttpClients
+{
+    public class MerchandiseHttpClient : IMerchandiseHttpClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public MerchandiseHttpClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<GetMerchResponse> GetMerch(long employeeId, string itemName, CancellationToken token)
+        {
+            //throw new System.NotImplementedException();
+            
+            using var response = await _httpClient.GetAsync("v1/api/merch", token);
+            return await response.Content.ReadFromJsonAsync<GetMerchResponse>(cancellationToken: token);
+        }
+
+        public async Task<GetOrderStateResponse> GetMerchOrderState(long id, CancellationToken token)
+        {
+           // throw new System.NotImplementedException();
+           
+           using var response = await _httpClient.GetAsync("v1/api/merch/check", token);
+           return await response.Content.ReadFromJsonAsync<GetOrderStateResponse>(cancellationToken: token);
+        }
+    }
+}
