7d408d7edb3682efc1c1046c7abc75a8e31c53dd
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2022-09-19 08:09:53 +0300

    add IMerchandiseService

---
+++ MerchandiseServiceWebAPI/Services/Interfaces/IMerchandiseService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MerchandiseServiceModels;
+
+namespace MerchandiseServiceWebAPI.Services.Interfaces
+{
+    public interface IMerchandiseService
+    {
+        Task<GetMerchResponse> GetMerchandise(GetMerchRequest request, CancellationToken _);
+        Task<GetOrderStateResponse> GetMerchandiseOrderState(GetOrderStateRequest request, CancellationToken _);
+    }
+}
